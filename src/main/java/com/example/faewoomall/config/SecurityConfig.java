2b54a5c20105145385ce4527a9fb11fbc2cd3973
byeunghoon Kang <qudgns119@gmail.com>
2024-08-25 03:42:18 +0900

    Fix: import 수정

--- src/main/java/com/example/faewoomall/config/SecurityConfig.java
+++ src/main/java/com/example/faewoomall/config/SecurityConfig.java
@@ -1,12 +1,11 @@
-package com.example.shoppingmall.config;
+package com.example.faewoomall.config;
 
-import com.example.shoppingmall.handler.CustomOAuth2SuccessHandler;
-import com.example.shoppingmall.service.CustomOauth2UserService;
+import com.example.faewoomall.handler.CustomOAuth2SuccessHandler;
+import com.example.faewoomall.service.CustomOauth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;

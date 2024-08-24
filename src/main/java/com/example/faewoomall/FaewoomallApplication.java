c53e1ad0ce0a961730a3a9738d4e7aeef22585ac
byeunghoon Kang <qudgns119@gmail.com>
2024-08-24 23:12:15 +0900

    Chore: 초기 설정 세팅

---
+++ src/main/java/com/example/faewoomall/FaewoomallApplication.java
@@ -0,0 +1,13 @@
+package com.example.faewoomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaewoomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaewoomallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/faewoomall/FaewoomallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faewoomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaewoomallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
